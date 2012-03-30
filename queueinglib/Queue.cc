70f5a9286e1c9919c2badb012d2af88f20591287
matthies <s.matthies@gsi.de>
2012-03-30 10:02:34 +0200

    cleaned up project

--- queueinglib/Queue.cc
+++ queueinglib/Queue.cc
@@ -39,58 +39,166 @@ void Queue::initialize()
     fifo = par("fifo");
     capacity = par("capacity");
     queue.setName("queue");
+
+    _capacity = par("capacity");
+
+    WATCH(numServed);
+    numServed = 0;
+
+    WATCH(numQueued);
+    numQueued = 0;
+
+    const char * algName = par("schedulingAlgorithm");
+    std::cout << this->getName() << " scheduling algorithm " << algName << std::endl;
+    if (strcmp(algName, "none") == 0) {
+		_scheduling = 0;
+		std::cout << "queue none" << std::endl;
+		Useful::getInstance()->appendToFile("out.txt", "queue none");
+	} else if (strcmp(algName, "priority") == 0) {
+		_scheduling = 1;
+		std::cout << "queue priority" << std::endl;
+		Useful::getInstance()->appendToFile("out.txt", "queue priority");
+	} else if (strcmp(algName, "feedback") == 0) {
+		_scheduling = 2;
+		std::cout << "queue feedback" << std::endl;
+		Useful::getInstance()->appendToFile("out.txt", "queue feedback");
+	} /*else if (strcmp(algName, "original") == 0) {
+		_scheduling = 3;
+		std::cout << "queue original" << std::endl;
+		Useful::getInstance()->appendToFile("out.txt", "queue original");
+	}*/ else if (strcmp(algName, "7first") == 0) {
+		_scheduling = 4;
+		std::cout << "queue 7first" << std::endl;
+		Useful::getInstance()->appendToFile("out.txt", "queue 7first");
+	} else if (strcmp(algName, "feedback2") == 0) {
+		_scheduling = 5;
+		std::cout << "queue feedback2" << std::endl;
+		Useful::getInstance()->appendToFile("out.txt", "queue feedback2");
+	}
+
 }
 
 void Queue::handleMessage(cMessage *msg)
 {
-    if (msg==endServiceMsg)
-    {
-        endService( packetServiced );
-        if (queue.empty())
-        {
-            packetServiced = NULL;
-            emit(busySignal, 0);
-        }
-        else
-        {
-            packetServiced = getFromQueue();
-            emit(queueLengthSignal, length());
-            simtime_t serviceTime = startService( packetServiced );
-            scheduleAt( simTime()+serviceTime, endServiceMsg );
-        }
-    }
-    else
-    {
-        Packet *packet = check_and_cast<Packet *>(msg);
-        arrival(packet);
+    Packet *packet = check_and_cast<Packet *>(msg);
+    packet->setTimestamp();
+    int k;
 
-        if (!packetServiced)
-        {
-        // processor was idle
-            packetServiced = packet;
-            emit(busySignal, 1);
-            simtime_t serviceTime = startService( packetServiced );
-            scheduleAt( simTime()+serviceTime, endServiceMsg );
-        }
-        else
-        {
-            // check for container capacity
-            if (capacity >=0 && queue.length() >= capacity)
-            {
-                EV << "Capacity full! Packet dropped.\n";
-                if (ev.isGUI()) bubble("Dropped!");
-                emit(droppedSignal, 1);
-                delete packet;
-                return;
-            }
-            queue.insert( packet );
-            emit(queueLengthSignal, length());
-            packet->setQueueCount(packet->getQueueCount() + 1);
-        }
-    }
+    switch(_scheduling) {
+    case 0:	// none
+    	// use with WRS.ned
+    	// send through without queueing
+		send(packet, "out", 0);
+		numServed++;
+    	break;
+    case 1:	// prority
+    	// use with WRS1.ned
+    	//std::cout << this->getName() << " priority" << std::endl;
+        //std::cout << "capacity " << capacity << " " << this->getName() << " queue size " << determineQueueSize() << std::endl;
+    	checkCapacityAndQueue(msg);
+    	break;
+    case 2:	// feedback
+    	//std::cout << this->getName() << " feedback" << std::endl;
+    	//checkCapacityAndQueue(msg);
+    	send(packet, "out", 0);
+    	numServed++;
+    	break;
 
-    if (ev.isGUI()) getDisplayString().setTagArg("i",1, !packetServiced ? "" : "cyan3");
+	case 4:	// 7first
+		// use with WRS1.ned
+		//std::cout << this->getName() << " 7first" << std::endl;
+		if( packet->getPriority()==7 ){
+			// send through without queueing
+			send(packet, "out",0);
+			numServed++;
+		} else {
+			// enqueue if no idle server found
+			queue.insert(packet);
+			emit(queueLengthSignal, length());
+			packet->setQueueCount(packet->getQueueCount() + 1);
+		}
+		break;
+	case 5:	// feedback 2
+		//checkCapacityAndQueue(msg);
+		send(packet, "out", 0);
+		numServed++;
+		break;
+    default:
+    	break;
+    } // switch
 }
+
+void Queue::checkCapacityAndQueue(cMessage *msg) {
+	Packet *packet = check_and_cast<Packet *>(msg);
+	if( _capacity >=0 && determineQueueSize()>=_capacity ) {
+		EV << this->getName() << " full! Packet dropped.\n";
+		std::cerr << this->getName() << " full! Packet dropped." << std::endl;
+		if (ev.isGUI()) bubble("Dropped!");
+		emit(droppedSignal, 1);
+		delete msg;
+		return;
+	}
+	// Trigger Test
+	// queue everything until requested, truly passive queue
+	queue.insert(packet);
+	//std::cout << "queued: " << packet->getName() << std::endl;
+	emit(queueLengthSignal, length());
+	packet->setQueueCount(packet->getQueueCount() + 1);
+	numQueued++;
+
+	// change the icon color
+	if (ev.isGUI())
+		getDisplayString().setTagArg("i",1, queue.empty() ? "" : "cyan3");
+} // checkCapacityAndQueue()
+
+void Queue::request()
+{
+    Enter_Method("request()!");
+
+    ASSERT(!queue.empty());
+
+    Packet *packet;
+    if (fifo) {
+        packet = (Packet *)queue.pop();
+        std::cout << __FUNCTION__ << " pop: " << packet->getName() << std::endl;
+    } else {
+        packet = (Packet *)queue.back();
+        std::cout << __FUNCTION__ << " back: " << packet->getName() << std::endl;
+        // FIXME this may have bad performance as remove uses linear search
+        queue.remove(packet);
+    }
+    emit(queueLengthSignal, length());
+
+    packet->setQueueCount(packet->getQueueCount()+1);
+    simtime_t d = simTime() - packet->getTimestamp();
+    packet->setTotalQueueingTime(packet->getTotalQueueingTime() + d);
+    emit(queueingTimeSignal, d);
+
+    send(packet, "out");
+    numServed++;
+    //std::cout << "packet requested without gate " << std::endl;
+
+    if (ev.isGUI())
+        getDisplayString().setTagArg("i",1, queue.empty() ? "" : "cyan");
+} // request()
+
+int Queue::determineQueueSize() {
+	if( queue.length()>0 ) {
+		int size=0;
+		int queuelength = queue.length();	// TODO can the length change during size determination? I hope not!
+		// iterate queue without removing or changing objects inside
+		for( int i; i<queuelength; i++ ) {
+			Packet *j = check_and_cast<Packet *>(queue.get(i));
+			if( j!=NULL ) {
+				size += j->getSize();
+			}
+		}
+		return size;
+	} else {
+		//std::cout << "queue length 0" << std::endl;
+		return 0;
+	}
+} // determineQueueSize()
 
 Packet *Queue::getFromQueue()
 {
